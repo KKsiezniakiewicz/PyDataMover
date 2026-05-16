11086fd85c9aa8c512a13deb419a40f27aef5e31
KKsiezniakiewicz <trybik333@gmail.com>
2026-05-16 14:41:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 # Wersja zmodyfikowana na SERWERZE
 
 try:
-    print("Program csvmover działa poprawnie")
+    print("Program działa poprawnie")
 except Exception as e:
     print("Wystąpił błąd:", e)
